4e4bcb16f01747ab3d3358906bc695d23137c4d1
Mov4D <mov4d.community@gmail.com>
2022-10-31 15:18:51 +0300

    ct-15: utilites and models

---
+++ CalorieTracker/Models/Shadow.swift
@@ -0,0 +1,15 @@
+//
+//  Shadow.swift
+//  CalorieTracker
+//
+//  Created by Vadim Aleshin on 30.10.2022.
+//
+
+import UIKit
+
+struct Shadow {
+    let color: UIColor
+    let opacity: Float
+    let offset: CGSize
+    let radius: CGFloat
+}
---
+++ CalorieTracker/Utilities/Layer+addShadow.swift
@@ -0,0 +1,41 @@
+//
+//  Layer+addShadow.swift
+//  CalorieTracker
+//
+//  Created by Vadim Aleshin on 30.10.2022.
+//
+
+import UIKit
+
+extension CALayer {
+    func addShadow(shadow: Shadow, rect: CGRect, cornerRadius: CGFloat) {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            cornerRadius: cornerRadius
+        )
+       
+        let shadowLayer = CALayer()
+        shadowLayer.shadowPath = path.cgPath
+        shadowLayer.shadowColor = shadow.color.cgColor
+        shadowLayer.shadowOpacity = shadow.opacity
+        shadowLayer.shadowOffset = shadow.offset
+        shadowLayer.shadowRadius = shadow.radius
+        shadowLayer.frame = bounds
+        path.append(UIBezierPath(rect: CGRect(
+            origin: CGPoint(
+                x: bounds.origin.x - (shadow.radius + 10) / 2.0,
+                y: bounds.origin.y - (shadow.radius + 10) / 2.0
+            ),
+            size: CGSize(
+                width: bounds.width + shadow.radius + 20,
+                height: bounds.height + shadow.radius + 20
+            )
+        )))
+        
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        mask.fillRule = .evenOdd
+        shadowLayer.mask = mask
+        self.insertSublayer(shadowLayer, at: 0)
+    }
+}
